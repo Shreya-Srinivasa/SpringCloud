c60cdfd1456197d94a7242e44e383763b35003b6
Shreya-Srinivasa <coolshreyasrinivasa@gmail.com>
2018-10-17 12:52:48 +0530

    Multiple framework errors

--- SpringCloud/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ SpringCloud/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -10,9 +11,15 @@ public class ExchangeValue {
 	
 	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
+	
 	private BigDecimal conversionMultiple;
+	
 	private int port; 
 	
 	public ExchangeValue()
